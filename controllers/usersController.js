386b06ed675cec41de35ed2b31c8dd92aeea32fa
Maksymilian Jaron <maximilian.jaron@gmail.com>
2022-07-11 14:19:35 +0200

    updating and deleting only specific user records

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,34 +2,82 @@ const asyncHandler = require("express-async-handler");
 const { v4: uuidV4 } = require("uuid");
 const { default: mongoose } = require("mongoose");
 const userRecord = require("../models/usersRecordsModel");
+const User = require("../models/appUserModel");
 
 const getUsers = asyncHandler(async (req, res) => {
-  const userRecords = await userRecord.find({appUser: req.appUser.id});
+  const userRecords = await userRecord.find({ appUser: req.appUser.id });
   res.status(200).json(userRecords);
 });
-const updateUser = asyncHandler(async (req, res) => {
-  const updatedUser = await userRecord.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  );
-  res.status(200).json(updatedUser);
-});
+
 const addUser = asyncHandler(async (req, res) => {
   const newUser = await userRecord.create({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
     age: req.body.age,
-    appUser: req.appUser.id
+    appUser: req.appUser.id,
   });
 
   res.status(200).json(newUser);
 });
-const deleteUser = asyncHandler(async (req, res) => {
-  const deletedUser = await userRecord.findByIdAndDelete(req.params.id);
 
-  res.status(200).json({id: req.params.id});
+const updateUser = asyncHandler(async (req, res) => {
+  const userRecordToChange = await userRecord.findById(req.params.id);
+
+  if (!userRecordToChange) {
+    res.status(400);
+    throw new Error("User record not found");
+  }
+
+  const currentAppUser = await User.findById(req.appUser.id);
+
+  if (!currentAppUser) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  if (userRecordToChange.appUser.toString() !== currentAppUser.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  const updatedUserRecord = await userRecord.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+
+  res.status(200).json(updatedUserRecord);
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+  const deleteUserRecord = await userRecord.findById(req.params.id);
+
+  if (!deleteUserRecord) {
+    res.status(400);
+    throw new Error("User record not found");
+  }
+
+  const currentAppUser = await User.findById(req.appUser.id);
+  console.log(currentAppUser.id)
+  console.log(deleteUserRecord.appUser.toString())
+
+
+  if (!currentAppUser) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  if (deleteUserRecord.appUser.toString() !== currentAppUser.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  await deleteUserRecord.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
