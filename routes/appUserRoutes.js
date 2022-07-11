6d50b3c9d9fcdf887ffe6c71a5532a616455ef6c
Maksymilian Jaron <maximilian.jaron@gmail.com>
2022-07-11 09:52:53 +0200

    middleware auth

--- routes/appUserRoutes.js
+++ routes/appUserRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
-const router = express.Router();
+
+
 const {registerUser, loginUser, getMe} = require("../controllers/appUsersController");
+const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
+
+
 router.post("/", registerUser)
 router.post("/login", loginUser)
-router.get("/me", getMe)
+router.get("/me", protect, getMe)
 
 module.exports = router;
