b42c6349a8cd9d0ed69acc7cbbe1f5679b50a773
Maksymilian Jaron <maximilian.jaron@gmail.com>
2022-07-17 19:20:52 +0200

    added heroku url

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connectDB = require("./config/db");
 connectDB()
 const app = express();
 const PORT = process.env.PORT || 5000;
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000", "https://users-manage-dashboard.herokuapp.com/"];
 
 const options = {
   origin: allowedOrigins,
