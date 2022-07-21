d8f61a208638d071590ed05033c4c32e5081dca8
Maksymilian Jaron <maximilian.jaron@gmail.com>
2022-07-21 16:14:19 +0200

    sockets finally connected

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
+const socket = require("socket.io");
 
-connectDB()
+connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
-const allowedOrigins = ["http://localhost:3000", "https://celadon-empanada-d6316b.netlify.app"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://celadon-empanada-d6316b.netlify.app",
+];
 
 const options = {
   origin: allowedOrigins,
-  credentials: true
+  credentials: true,
 };
 
 app.use(cors(options));
@@ -19,9 +23,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/appusers', require('./routes/appUserRoutes'));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/appusers", require("./routes/appUserRoutes"));
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`New Server running on port ${PORT}`);
+});
+
+const io = socket(server, {
+  // path: "/api/users",
+  cors: {
+    origin: allowedOrigins,
+  },
+});
+io.on("connection", (socket) => {
+  console.log("New client connected");
 });
